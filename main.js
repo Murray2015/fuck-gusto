5fe8871a0bdb483a83705870a3d5c0a3ce0d5965
Murray Hoggett <murrayhoggett@gmail.com>
2020-02-23 10:04:19 +0000

    Front end and fetch now linked and working. Next up, page design by hand and then page styling.

--- main.js
+++ main.js
@@ -5,11 +5,17 @@ async function processData(recipeObj) {
     recipe[0].steps.forEach(x => console.log(x.step))
 }
 
-async function requestRecipeId() {
+async function requestRecipeId(diet = null, intolerances = null, cusine = null) {
     // Request all recipes for a certain meal and cuisine type
     const meal = "main"; // appetizer, main course, desert
-    const cuisine = "italian"
-    const res = await fetch(`https://api.spoonacular.com/recipes/search?&instructionsRequired=true&type=${meal}&cuisine=${cuisine}&apiKey=29f96c91e7b64bd690b4d3bbb695847b`)
+    const dietString = diet ? `&diet=${diet}` : "";
+    const cusineString = cusine ? `&cuisine=${cusine}` : "";
+    const intolerancesString = intolerances ? intolerances.reduce((a, c) => {
+        return a + c + ","
+    }, "&intolerances=").slice(0, -1) : "";
+    const fetchString = `https://api.spoonacular.com/recipes/search?&instructionsRequired=true&type=${meal}` + cusineString + dietString + intolerancesString + `&apiKey=29f96c91e7b64bd690b4d3bbb695847b`
+    console.log(fetchString)
+    const res = await fetch(fetchString)
     const recipe = await res.json();
     console.log(recipe)
     processData(recipe);
