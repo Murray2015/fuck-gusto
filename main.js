039edc5407be03d1055533186ecfe9c7399fd21c
Murray Hoggett <murrayhoggett@gmail.com>
2020-02-23 09:11:47 +0000

    Basic front end working. Next - link with the fetch requests.

--- main.js
+++ main.js
@@ -17,6 +17,6 @@ async function requestRecipeId() {
 
 // async function 
 
-requestRecipeId()
+// requestRecipeId()
 
 // fetch("https://api.spoonacular.com/food/products/search?query=yogurt&number=2&apiKey=29f96c91e7b64bd690b4d3bbb695847b").then(res => res.json()).then(data => console.log(data))
